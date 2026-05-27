fb63635323a5bb7c7fbb48e1f0e2900e4e0e627b
nuvemnm <matheuscarv010@gmail.com>
2026-05-27 20:39:07 -0300

    feat: aviao direcionado

--- flaskr/static/js/widgets/MapWidget.js
+++ flaskr/static/js/widgets/MapWidget.js
@@ -1,193 +1,77 @@
-export default class MapWidget {
+// Lembre-se de importar a classe pai (ajuste o caminho se necessário)
+import BaseWidget from './BaseWidget.js';
+
+export default class MapWidget extends BaseWidget {
     constructor(title, idContainerDestino) {
-        this.title = title;
-        this.containerDestino = document.getElementById(idContainerDestino);
+        super(title, idContainerDestino);
+
         this.aviaoFocado = null;
-
-        this.AirplaneIcon = L.Icon.extend({
-            options: {
-                iconUrl: '/static/img/plane.png', 
-                // shadowUrl: 'caminho/para/sua/sombra.png',
-                iconSize:     [38, 38], 
-                iconAnchor:   [19, 19], 
-                popupAnchor:  [0, -20]
-            }
-        });
-        
         this.marcadoresAtivos = {};
-
-        this.element = document.createElement('div');
-        this.element.className = 'widget';
-
-        this.header = document.createElement('div');
-        this.header.className = 'widget-header';
-
-        this.titleSpan = document.createElement('span');
-        this.titleSpan.innerText = this.title;
-
-        this.closeWidget = document.createElement('div');
-        this.closeWidget.className = 'widget-close';
-        this.closeWidget.innerHTML = '&times;'; 
-
-        this.header.appendChild(this.titleSpan);
-        this.header.appendChild(this.closeWidget);
-
-        this.content = document.createElement('div');
-        this.content.className = 'widget-content';
+        this.ultimasCoordenadas = {};
+        this.ultimosAngulos = {};
 
         this.mapContainer = document.createElement('div');
-
-        // Gera um ID único e aleatório para este mapa (ex: mapa-a8b2x9)
         this.mapId = 'mapa-' + Math.random().toString(36).substring(2, 9);
         this.mapContainer.id = this.mapId;
         this.mapContainer.className = 'map-container';
 
         this.content.appendChild(this.mapContainer);
 
-        this.resize_handle = document.createElement('div');
-        this.resize_handle.className = 'widget-resize-handle';
-
-
-        this.element.appendChild(this.header);
-        this.element.appendChild(this.content);
-        this.element.appendChild(this.resize_handle)
-
-        //configuracao de eventos
-        this.configEvents();
-    }
-
-    setContent(htmlString) {
-        this.content.innerHTML = htmlString;
-    }
-
-    configEvents(){
-        let isDragging = false;
-        let isResizing = false;
-        let startX, startY, initialX, initialY, initialWidth, initialHeight;
-        let gridSize = 20;
-
-        //Lógica de fechamento
-        this.closeWidget.addEventListener('mousedown', (e) => {
-            e.stopPropagation(); 
-        });
-
-        this.closeWidget.addEventListener('click', () => {
-            this.element.remove(); 
-        });
-
-        //Lógica de resize 'snap-to-grid'
-        this.header.addEventListener('mousedown',(e) => {
-            isDragging = true;
-
-            startX = e.clientX;
-            startY = e.clientY;
-
-            initialX = this.element.offsetLeft;
-            initialY = this.element.offsetTop;
-        })
-
-        //Lógica de arraste 'snap-to-grid'
-        this.resize_handle.addEventListener('mousedown', (e) => {
-            e.stopPropagation(); 
-            e.preventDefault();
-            
-            isResizing = true;
-            startX = e.clientX;
-            startY = e.clientY;
-            initialWidth = this.element.offsetWidth;
-            initialHeight = this.element.offsetHeight;
-        });
-
-        document.addEventListener('mousemove',(e) => {
-            if (isDragging){
-                let rawX = initialX + e.clientX - startX;
-                let rawY = initialY + e.clientY - startY;
-
-                let snappedX = Math.round(rawX / gridSize) * gridSize;
-                let snappedY = Math.round(rawY / gridSize) * gridSize;
-
-                const limiteLargura = this.containerDestino.clientWidth;
-                const limiteAltura = this.containerDestino.clientHeight;
-
-                const maxX = limiteLargura - this.element.offsetWidth;
-                const maxY = limiteAltura - this.element.offsetHeight;
-
-                snappedX = Math.max(0, Math.min(snappedX, maxX));
-                snappedY = Math.max(0, Math.min(snappedY, maxY));
-
-                this.element.style.left = `${snappedX}px`;
-                this.element.style.top = `${snappedY}px`;
+        const observer = new ResizeObserver(() => {
+            if (this.map) {
+                this.map.invalidateSize();
             }
-            if (isResizing){
-                let rawWidth = initialWidth + (e.clientX - startX);
-                let rawHeight = initialHeight + (e.clientY - startY);
-
-                let snappedWidth = Math.round(rawWidth / gridSize) * gridSize;
-                let snappedHeight = Math.round(rawHeight / gridSize) * gridSize;
-
-                const maxWidth = this.containerDestino.clientWidth - this.element.offsetLeft;
-                const maxHeight = this.containerDestino.clientHeight - this.element.offsetTop;
-
-                snappedWidth = Math.max(260, Math.min(snappedWidth, maxWidth));
-                snappedHeight = Math.max(200, Math.min(snappedHeight, maxHeight));
-
-                this.element.style.width = `${snappedWidth}px`;
-                this.element.style.height = `${snappedHeight}px`;
-            }
-        })
-
-        document.addEventListener('mouseup',() =>{
-            isDragging = false;
-            if (isResizing) {
-                isResizing = false;
-
-                if (this.map) {
-                    this.map.invalidateSize();
-                }
-            }
-        })
-
-
+        });
+        observer.observe(this.content);
     }
 
     render() {
-        if (this.containerDestino) {
-            this.containerDestino.appendChild(this.element);
+        // 1. Manda o BaseWidget anexar o HTML na tela
+        super.render();
+        
+        // 2. Inicializa o Leaflet (pois ele precisa que a div já exista na tela)
+        setTimeout(() => {
+            this.map = L.map(this.mapId).setView([-19.9167, -43.9345], 13);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(this.map);
             
-            setTimeout(() => {
-                // As coordenadas [-19.9167, -43.9345] centralizam em Belo Horizonte, com zoom 13
-                this.map = L.map(this.mapId).setView([-19.9167, -43.9345], 13);
-
-                // Adiciona os blocos de imagens (ruas, bairros) gratuitos do OpenStreetMap
-                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    maxZoom: 19,
-                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-                }).addTo(this.map);
-                
-                const soltarCamera = () => {
-                    if (this.aviaoFocado !== null) {
-                        
-                        let idDoAviaoSeguido = this.aviaoFocado;
-                        
-                        this.aviaoFocado = null; 
-                        
-                        let aviao = this.marcadoresAtivos[idDoAviaoSeguido];
-                        if (aviao) {
-                            aviao.slideTo(aviao.getLatLng(), {
-                                duration: 0,
-                                keepAtCenter: false
-                            });
-                        }
+            const soltarCamera = () => {
+                if (this.aviaoFocado !== null) {
+                    let idDoAviaoSeguido = this.aviaoFocado;
+                    this.aviaoFocado = null; 
+                    
+                    let aviao = this.marcadoresAtivos[idDoAviaoSeguido];
+                    if (aviao) {
+                        aviao.slideTo(aviao.getLatLng(), {
+                            duration: 0,
+                            keepAtCenter: false
+                        });
                     }
-                };
-                
-                this.map.on('dragstart', soltarCamera);
-                this.map.on('click', soltarCamera);
-            }, 0);
+                }
+            };
+            
+            this.map.on('dragstart', soltarCamera);
+            this.map.on('click', soltarCamera);
+        }, 0);
+    }
 
-        } else {
-            console.error("Contêiner de destino não encontrado");
+    calcularAnguloDirecao(idVoo,latAnterior, lngAnterior, latAtual, lngAtual) {
+        if (latAnterior === latAtual && lngAnterior === lngAtual) {
+            return this.ultimosAngulos[idVoo] || 0;
         }
+
+        let dx = lngAtual - lngAnterior;
+        let dy = latAtual - latAnterior;
+
+        let anguloGraus = Math.atan2(dy, dx) * (180 / Math.PI);
+
+        let anguloAjustado = 90 - anguloGraus;
+        this.ultimosAngulos[idVoo] = anguloAjustado;
+        
+        return anguloAjustado;
     }
 
     atualizarAviao(dadosDoVoo) {
@@ -197,41 +81,70 @@ export default class MapWidget {
         let lat = dadosDoVoo.lat;
         let lng = dadosDoVoo.lng;
 
+        let coordAnterior = this.ultimasCoordenadas[idVoo] || { lat: lat, lng: lng };
+        
+        let angulo = this.calcularAnguloDirecao(idVoo, coordAnterior.lat, coordAnterior.lng, lat, lng);
+        
+        this.ultimasCoordenadas[idVoo] = { lat: lat, lng: lng };
+
+        const gerarSVG = (cor, rotacao) => `
+            <svg style="transform: rotate(${rotacao}deg); transform-origin: center; transition: transform 2s linear;" 
+                 viewBox="0 0 24 24" width="38" height="38" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 16v-2l-8-5V3.5c0-.83-.67-1.5-1.5-1.5S10 2.67 10 3.5V9l-8 5v2l8-2.5V19l-2 1.5V22l3.5-1 3.5 1v-1.5L13 19v-5.5l8 2.5z" 
+                      fill="${cor}" />
+            </svg>
+        `;
+
         if (this.marcadoresAtivos[idVoo]) {
-            
             let deveCentralizar = (this.aviaoFocado === idVoo);
 
             this.marcadoresAtivos[idVoo].slideTo([lat, lng], {
                 duration: 2000,       
                 keepAtCenter: deveCentralizar,   
             });
+
             let textoAtualizado = `<b>${idVoo}</b><br>${dadosDoVoo.lat}<br>${dadosDoVoo.lng}<br>${dadosDoVoo.altitude} ft`;
-            
             this.marcadoresAtivos[idVoo].setTooltipContent(textoAtualizado);
             
+            let novaCor = dadosDoVoo.altitude < 11000 ? "red" : "blue";
+            let iconeAtualizado = L.divIcon({ 
+                html: gerarSVG(novaCor, angulo), // Usa a função de gerar o SVG
+                className: '', 
+                iconSize: [38,38], 
+                iconAnchor: [19,19] 
+            });
+            
+            this.marcadoresAtivos[idVoo].setIcon(iconeAtualizado);
+            
         } else {
-            let iconeDoVoo = new this.AirplaneIcon();
+            let corInicial = dadosDoVoo.altitude < 11000 ? "red" : "blue";
+
+            let icone = L.divIcon({
+                html: gerarSVG(corInicial, angulo),
+                className: '',
+                iconSize: [38, 38],
+                iconAnchor: [19, 19]
+            });
+
             let textoLateral = `<b>${idVoo}</b><br>${dadosDoVoo.lat}<br>${dadosDoVoo.lng}<br>${dadosDoVoo.altitude} ft`;
-            let novoMarcador = L.marker([lat, lng], {icon: iconeDoVoo})
+            
+            let novoMarcador = L.marker([lat, lng], {icon: icone})
                 .bindTooltip(textoLateral, { 
-                    permanent: false,       // Mantém o texto sempre visível
-                    direction: 'right',    // Posiciona o texto à direita do avião
-                    offset: [15, 0],       // Afasta o texto 15 pixels para não cobrir o desenho
-                    className: 'meu-tooltip' // Classe CSS opcional para você estilizar depois
+                    permanent: false,      
+                    direction: 'right',    
+                    offset: [15, 0],       
+                    className: 'meu-tooltip' 
                 })
                 .addTo(this.map);
             
             novoMarcador.on('click', () => {
-                
                 if (this.aviaoFocado === idVoo) {
                     console.log(`Parando de focar no avião: ${idVoo}`);
                     this.aviaoFocado = null;
-                } 
-                else {
+                } else {
                     console.log(`Focando no avião: ${idVoo}`);
                     this.aviaoFocado = idVoo; 
                 }
-                
             });
             
             this.marcadoresAtivos[idVoo] = novoMarcador;
@@ -240,7 +153,6 @@ export default class MapWidget {
 
     update(dados) {
         if (dados && dados.avioes) {
-            
             for (let aviaoAtual of dados.avioes) {
                 this.atualizarAviao(aviaoAtual);
             }
